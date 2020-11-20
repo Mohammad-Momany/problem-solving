65539af1863ed93f6f2597a4bba5d4bd167f2b3f
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-11-20 13:06:18 +0200

    Q05,Q06

--- Q04/main.js
+++ Q04/main.js
@@ -13,13 +13,29 @@ Have the function CodelandUsernameValidation(str) take the str parameter being p
 If the username is valid then your program should return the string true, otherwise return the string false.
 */
 
-function CodelandUsernameValidation(str) {
-  // YOUR CODE HERE
-}
+// function CodelandUsernameValidation(str) {
+//   if(str[str.length - 1].replace(/['"]+/g, '') === '_'){
+//     return false 
+//   } 
+//   if(!(str[0].replace(/['"]+/g, '')) ===  ){}
+//   if (str.length > 3 && str.length < 26  ) {
+//   return true
+  
+// }
+// }
 
 /* 
+
+
 Examples:
 CodelandUsernameValidation('aa_'); // => false
 CodelandUsernameValidation('u__hello_world123'); // => true
 
 */
+
+
+
+
+// the first characters split[split.length - 1]
+// the last characters split[0]
+/// remove the quotationsome ( .replace(/['"]+/g, ''))
---
+++ Q06/main.js
@@ -0,0 +1,25 @@
+console.log('Problem Solving Q: 6');
+
+/* Even or Not */
+
+/*  
+Write a function that returns true if a number is even, and false otherwise.
+*/
+
+function evenOrNot(num) {
+  
+  if( num % 2 === 0 ){
+    return true
+  }else{
+    return false}
+  
+}
+
+/* 
+Examples:
+evenOrNot(1) // => false
+evenOrNot(2) // => true
+evenOrNot(5) // => false
+evenOrNot(99) // => false
+evenOrNot(0) // => true
+*/
