3d3be9f761965d1f2fa6d8a5c20c1114b0fd2bc8
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-11 00:47:53 +0200

    Q15 15min

---
+++ Q15/main.js
@@ -0,0 +1,27 @@
+console.log("Problem Solving Q:15 ");
+
+/*  commonCharacters*/
+
+/*  
+Write a function that accepts two strings as arguments, and returns only the characters that are common to both strings.
+Your function should return the common characters in the same order that they appear in the first argument. 
+Do not return duplicate characters and ignore whitespace in your returned string.
+*/
+
+function commonCharacters(first, second) {
+  let arr = [];
+  let first1 = first.split(" ").join("").split("");
+  let second1 = second.split(" ").join("").split("");
+
+  first1.filter((char) => {
+    if (second1.includes(char)) {
+      if (!arr.includes(char)) arr.push(char);
+    }
+  });
+  console.log(arr.join(""));
+  return arr.join("");
+}
+
+Examples: commonCharacters("abc", "abc"); // => 'abc'
+commonCharacters("What is love?", "Baby don't hurt me"); // => 'hatoe'
+commonCharacters("Riding on a buffalo makes me more approachable", "so what"); // => 'oash'
