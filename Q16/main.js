9742740ff458e1763155389d53bac1ba69c5f0e8
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-11 00:58:26 +0200

    Q16 10min

---
+++ Q16/main.js
@@ -0,0 +1,19 @@
+console.log("Problem Solving Q:16 ");
+
+/* evenOccurrence */
+
+/*  
+Find the first item that occurs an even number of times in an array.
+*/
+
+function evenOccurrence(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] % 2 === 0 && arr[i] === arr[i + 1]) {
+      return arr[i];
+    }
+  }
+}
+
+// Examples: evenOccurrence([1, 2, 6, 6]); // => 6
+// evenOccurrence([1, 2, 9, 8, 8, 6, 6]); // => 8
+// evenOccurrence([1, 9, 7, 3, 6, 6, 8, 9, 9]); // => 6
