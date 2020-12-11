91189bcaee19aa7101294f33184261b125365b0b
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-11 21:20:40 +0200

    Q9

---
+++ Q09/main.js
@@ -0,0 +1,37 @@
+console.log("Problem Solving Q: 9");
+
+/* Longest Sequence */
+
+/*  
+Write a function that returns the length of the longest sequence of 1s in an array of 1s and 0s.
+*/
+
+function longestSequence(array) {
+  let longest = 0;
+  let count = 0;
+  array.forEach(num => {
+    if (num !== 1) {
+      if (count > longest) {
+        longest=count;
+      }
+      count = 0;
+    } else {
+      count += 1
+    }
+  });
+      if (longest < count) {
+        longest = count;
+      }
+  return longest;
+}
+
+/* 
+Examples:
+longestSequence([0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1]) // => 4
+longestSequence([1, 1, 0, 1, 1, 0, 1, 0, 1, 1]) // => 2
+longestSequence([1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1]) // => 3
+longestSequence([1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1]) // => 4
+longestSequence([1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1]) // => 5
+longestSequence([1, 0, 0, 1]) // => 1
+longestSequence([0, 0, 0, 0]) // => 'There is no one sequenced'
+*/
