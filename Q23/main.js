5eb5f1256227ff8f932ab618e6b221a057f3af1b
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-12 03:47:38 +0200

    Q23

--- Q23/main.js
+++ Q23/main.js
@@ -9,12 +9,12 @@ If the url does not contain a query string, return undefined
 
 function parseQueryString(url) {
   let desired1 = (url).replace((/https?:\/\/[^\?]+/g), '')
-  let desired = desired1.replace(/[^\w\s]/gi, ",")
-  console.log(desired1.split(", "));
+  let desired = desired1.replace(/[^\w\s]/gi, " ").split(" ")
+return desired
 }
 
-parseQueryString("http://example.com?a=hello&b=99") // [ [ "a", "hello" ], [ "b", "99" ] ]
 /* 
+parseQueryString("http://example.com?a=hello&b=99") // [ [ "a", "hello" ], [ "b", "99" ] ]
 Examples:
 parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello world" ] ]
 parseQueryString("http://example.com") // undefined
