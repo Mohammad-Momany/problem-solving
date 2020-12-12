d8a329efa328db20f931f613c2b126c08b1f51e0
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-12 02:36:26 +0200

    Q25 5min

--- Q25/main.js
+++ Q25/main.js
@@ -6,13 +6,15 @@ console.log('Problem Solving Q: 25');
 Write a JavaScript function to check whether an `input` is an array or not.
 */
 
-function is_array() {
-  // YOUR CODE HERE
+function is_array(arr) {
+  if (Array.isArray(arr)) {
+   return true
 }
-
+return false
+}
+ 
 /* 
 Examples:
-is_array('w3resource') => false
 is_array ([1, 2, 4, 0]) // => true
 
 */
