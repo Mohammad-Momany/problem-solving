99d981f5dbb902b8e4538522931a3fb63c715c3f
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-27 21:07:13 +0200

    Q6

--- 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
@@ -8,6 +8,10 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //for loop
+    //slice = size 
+    //slice the number put them in an array
+}
 
 module.exports = chunk;
