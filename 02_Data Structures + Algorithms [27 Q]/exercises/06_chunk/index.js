e7c3c5e07ef64e9f45643eee3a0dbb9c1744c194
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-28 17:14:13 +0200

    Q6

--- 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/06_chunk/index.js
@@ -9,16 +9,20 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    let concat = []
-    array.forEach(num => {
-          if (concat.length > 0){
-            splice = num.splice(size)
-            concat.push(splice)
-        }else if(concat[0].toString() === num.slice(size).toString()){
-            slice = num.slice(size)
-            concat.push(slice)
-        }
-    });
+  let concat = [];
+  array.forEach((num) => {
+    if (
+      concat.length === 0 ||
+      concat[0].toString() === array.slice(size).toString()
+    ) {
+      slice = array.slice(0, size);
+      concat.push(slice);
+    } else if (concat.length > 0) {
+      splice = array.splice(0, size);
+      concat.push(splice);
+    }
+  });
+  console.log(concat);
 }
 
 module.exports = chunk;
