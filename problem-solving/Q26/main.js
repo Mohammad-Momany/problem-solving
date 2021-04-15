dbc3f942ca390af940be58179f5eb1fee3759708
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-04-15 13:00:00 +0300

    Q26

--- problem-solving/Q26/main.js
+++ problem-solving/Q26/main.js
@@ -1,18 +1,16 @@
 console.log('Problem Solving Q:26 ');
 
-/*  */
-
 /*  
 Write a JavaScript function to clone an array.
 */
 
 function array_Clone(arr) {
-  const clone = [].concat(arr) 
-  return clone
+    const clone = [...arr]
+    return clone
 }
 
-array_Clone ([1,2,3]) // =>[1,2,3]
 /* 
 Examples:
+array_Clone([1, 2, 3]) // =>[1,2,3]
 array_Clone ([1,9,8]) // =>[1,9,8]
 */
