29c5775d6171f207033b3a9c11d70dcfb19be513
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-04-15 12:56:19 +0300

    Q22

--- problem-solving/Q22/main.js
+++ problem-solving/Q22/main.js
@@ -9,17 +9,18 @@ console.log("Problem Solving Q: 22");
 */
 
 function characterFrequency(str) {
-  str = str.split("").sort();
-  let obj = {};
-  str.forEach((char) => {
-    obj[char] ? (obj[char] += 1) : (obj[char] = 1);
-  }); 
-  let entries = Object.entries(obj);
- 
+    str = [...str].sort();
+    let = {};
+    str.reduce((obj, char) => {
+        obj[char] = obj[char] + 1 || 1
+        return obj
+    }, {});
+    let entries = Object.entries(obj);
+
     return entries.sort((num1, num2) => num2[1] - num1[1])
-  
+
 }
- 
+
 /* 
 Examples:
 characterFrequency('mississippi') // =>  [ ['i', 4],['s', 4],  ['p', 2],  ['m', 1]  ]
