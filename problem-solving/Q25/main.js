1d32516b025a388cfac3bd38d5951d10974668a3
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-04-15 12:58:52 +0300

    Q25

--- problem-solving/Q25/main.js
+++ problem-solving/Q25/main.js
@@ -7,9 +7,8 @@ Write a JavaScript function to check whether an `input` is an array or not.
 */
 
 function is_array(arr) {
-  return Array.isArray(arr)
+    return arr.constructor === Array
 }
- 
 /* 
 Examples:
 is_array ([1, 2, 4, 0]) // => true
