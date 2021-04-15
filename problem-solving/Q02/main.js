6345f9f947ec7badaf2fe8c1217f54607e666424
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-04-15 08:01:49 +0300

    Q2

--- problem-solving/Q02/main.js
+++ problem-solving/Q02/main.js
@@ -3,22 +3,33 @@ console.log("Problem Solving Q: 2 ");
 /* Longest Word 2 */
 
 /*  
-Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
+If there are two or more words that are the same length, return the first word from the string with that length.
+Ignore punctuation and assume sen will not be empty.
 */
 
 function LongestWord_2(sen) {
-  if (sen.length === 0) {
-    return "Zero Words";
-  }
-  let output = "";
-  const words = sen.replace(/[^a-z]+/gi, " ").split(" ");
-
-  words.forEach((word) => {
-    if (word.length > output.length) {
-      output = word;
+    if (!sen) {
+        return "Zero Words";
     }
-  });
-  return output;
+
+    let clearString = ''
+    let words = []
+    for (let i = 0; i < sen.length; i++) {
+        if (sen[i - 1] === " ") {
+            words.push(clearString)
+            clearString = ""
+        }
+        if (sen[i] >= 'a' && sen[i] <= 'z') {
+            clearString += sen[i]
+        }
+    }
+    return clearString.reduce((longestWord, word) => {
+        if (word.length > longestWord.length) {
+            longestWord = word
+        }
+        return longestWord
+    }, '')
 }
 
 /* 
